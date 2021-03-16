cf4730ece485df81924c04fbe497d10e8a88de42
Exonymus <exonymus@icloud.com>
2021-03-16 14:59:20 +0300

    update

--- Lab2/Lab2_Task_2.2/Lab2_Task_2.2/test.c
+++ Lab2/Lab2_Task_2.2/Lab2_Task_2.2/test.c
@@ -9,7 +9,7 @@ int main()
     float x = 0.5;
     for (; x < 10; x += 0.1)
     {
-        assert(trunc(sum(x,17) * 100) - trunc(sinf(x) * 100) < pow(10, -9));
+        assert(sum(x,17) - sinf(x) < pow(10, -3));
         printf("%f - valid\n", x);
     }
     return 0;
