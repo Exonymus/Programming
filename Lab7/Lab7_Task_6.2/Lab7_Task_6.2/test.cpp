21b65bee0a65d363742ad08bad4ac7cffec308a6
Exonymus <exonymus@icloud.com>
2021-05-30 17:15:17 +0300

    Update test.cpp

--- Lab7/Lab7_Task_6.2/Lab7_Task_6.2/test.cpp
+++ Lab7/Lab7_Task_6.2/Lab7_Task_6.2/test.cpp
@@ -9,39 +9,43 @@ int main()
     DealList dBase;
     
     assert(baseCheck(gBase, dcBase) == 1);
-    cout << "Test 1 - Valid\n";
-    
     assert(gBase.getHead() == NULL);
-    cout << "Test 2 - Valid\n";
     
     ShopGoods temp;
     gBase.add(temp);
     
     assert(gBase.getHead() != NULL);
-    cout << "Test 3 - Valid\n";
-    
     assert(baseCheck(gBase, dcBase) == 1);
-    cout << "Test 4 - Valid\n";
     
     DiscountCard tempc;
     dcBase.add(tempc);
     
     assert(baseCheck(gBase, dcBase) == 0);
-    cout << "Test 5 - Valid\n";
     
     assert(fileRead(gBase, dcBase, dBase) == 0);
-    cout << "Test 6 - Valid\n";
     
     Deal t;
     t.Date.Day = 0;
     assert(t.completeDate() == "ERROR");
-    cout << "Test 7 - Valid\n";
+    
+    t.Date.Day = 2;
+    t.Date.Month = 0;
+    assert(t.completeDate() == "ERROR");
     
     t.Date.Day = 5;
     t.Date.Month = 5;
     t.Date.Year = 0;
     assert(t.completeDate() == "ERROR");
-    cout << "Test 8 - Valid\n";
+    
+    t.Date.Day = 5;
+    t.Date.Month = 0;
+    t.Date.Year = 5;
+    assert(t.completeDate() == "ERROR");
+    
+    t.Date.Day = 0;
+    t.Date.Month = 5;
+    t.Date.Year = 5;
+    assert(t.completeDate() == "ERROR");
     
     t.Date.Day = 5;
     t.Date.Month = 5;
